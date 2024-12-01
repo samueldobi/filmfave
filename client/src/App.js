24e60f0d5e67ce54ba5256c851f19fe8f16d17a2
samueldob <frankdobisamuel@gmail.com>
2024-12-01 01:47:27 +0100

    added react bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import Basic from './components/Basic';
+import Hero from './components/Hero';
 
 function App() {
+  // const [backendData, setBackendData] =  useState([{}]);
+
+  // useEffect(()=>{
+  //   fetch("/api").then(
+  //     response => response.json()
+  //   ).then(
+  //     data =>{
+  //       setBackendData(data)
+  //     }
+  //   )
+  // }, [])
   return (
-    <div>App</div>
+    <>
+    <Basic/>
+    <div className="content-body">
+    <Hero />
+    </div>
+    </>
   )
 }
 
---
+++ client/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='hero-section'>
+        <div className="container  hero-body">
+            <div className="row">
+                <div className="">hi</div>
+                <div className="">hi</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
