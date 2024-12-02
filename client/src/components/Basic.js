3a916bfebd46c97f0ee74c1c02ddbc861daba8fa
samueldob <frankdobisamuel@gmail.com>
2024-12-02 08:44:25 +0100

    working on the navbar

--- client/src/components/Basic.js
+++ client/src/components/Basic.js
@@ -7,20 +7,20 @@ function Basic() {
   return (
     <Navbar expand="lg" className=" navbar-body mx-auto ">
       <Container>
-        <Navbar.Brand href="#home">Filmfave</Navbar.Brand>
+        <Navbar.Brand href="#home" className='fw-bold'>Filmfave</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="me-auto ">
+            <Nav.Link href="#home">Top Movies</Nav.Link>
+            <Nav.Link href="#link">Top Actors</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item href="">Action</NavDropdown.Item>
+              <NavDropdown.Item href="">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item href="">
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
