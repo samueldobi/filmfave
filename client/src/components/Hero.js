cf5c28bf5fa718f5121eba450631d5db6ce5dcdb
samueldob <frankdobisamuel@gmail.com>
2024-12-03 15:00:22 +0100

    changed hero section alignment

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Hero = () => {
   return (
     <div className='hero-section'>
-        <div className="container  hero-body">
-            <div className="row">
-                <div className="">Your Personalized Movie Guide</div>
-                <div className="">FilmFave is your go-to for movie recommendations. Tell us what you like, and we'll suggest films you'll love.</div>
-            </div>
-        </div>
+      <Container fluid className='d-flex align-items-center justify-content-center min-vh-100'>
+        <Row>
+        <Col xs={12} className="text-center">
+          <p className='text-light text-center'>Your Personalized Movie Guide</p>
+          <p className='text-light'>FilmFave is your go-to for movie recommendations. Tell us what you like, and we'll suggest films you'll love.</p>
+        </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
