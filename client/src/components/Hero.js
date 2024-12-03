5d1312010d182ef2a41f96c16b2945b630875cc5
samueldob <frankdobisamuel@gmail.com>
2024-12-03 15:15:02 +0100

    added a movie button component

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Stack from 'react-bootstrap/Stack';
+import MovieBtn from './MovieBtn';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,12 +7,15 @@ import Col from 'react-bootstrap/Col';
 const Hero = () => {
   return (
     <div className='hero-section'>
-      <Container fluid className='d-flex align-items-center justify-content-center min-vh-100'>
+      <Container fluid className='d-flex align-items-center justify-content-center hero-body'>
         <Row>
         <Col xs={12} className="text-center">
-          <p className='text-light text-center'>Your Personalized Movie Guide</p>
-          <p className='text-light'>FilmFave is your go-to for movie recommendations. Tell us what you like, and we'll suggest films you'll love.</p>
+          <p className='text-light text-center fw-bold fs-1'>Your Personalized Movie Guide</p>
+          <p className='text-light fs-4'>FilmFave is your go-to for movie recommendations. Tell us what you like, and we'll suggest films you'll love.</p>
         </Col>
+       <Col className="text-center">
+       <MovieBtn/>
+       </Col>
         </Row>
       </Container>
     </div>
---
+++ client/src/components/MovieBtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const MovieBtn = () => {
+  return (
+    <div>
+         <Button variant="" className='movie-btn '>Get a Movie</Button>
+    </div>
+  )
+}
+
+export default MovieBtn
