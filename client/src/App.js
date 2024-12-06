fbde0bae6540ee648f35117beef1e3b894a7706e
samueldob <frankdobisamuel@gmail.com>
2024-12-06 07:19:59 +0100

    created the top movies component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom/cjs/react-router-dom.min";
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import Hero from './components/Hero';
 
@@ -20,9 +20,10 @@ function App() {
     <div className='app-body'>
     <NavigationBar/>
     <div className="content-body">
-    <Route>
-      <Hero />
-    </Route>
+      <Routes>
+          <Route path='/' element = {<Hero /> }/>
+      </Routes>
+ 
     </div>
     </div>
     </Router>
