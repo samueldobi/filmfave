a8aad4bca2ee3590a3c620a27f9d692fbf9f7c9b
samueldob <frankdobisamuel@gmail.com>
2024-12-08 08:39:35 +0100

    added pagination to topmovies component

--- client/src/components/TopMovies.js
+++ client/src/components/TopMovies.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 import MovieCard from './Movies/MovieCard';
-import {Container,Row, Col} from 'react-bootstrap';
+import {Container,Row, Col, Pagination} from 'react-bootstrap';
 
 
 const TopMovies = () => {
-    // const [backendData, setBackendData] =  useState([]);
-    const [movies, setMovies] = useState([]);
-    // const [movieTitle, setMovieTitle] = useState([]);
-    // const [movieImage, setMovieImage] = useState('');
+  const [movies, setMovies] = useState([]);
 
+    const [currentPage, setCurrentPage] =  useState(1);
+    const moviesPerPage = 3;
+    // Calculate the total number of pages
+    const totalPages = Math.ceil(movies.length / moviesPerPage)
+    // Get the movie for the current page
+    const currentMovies = movies.slice(
+      (currentPage - 1) * moviesPerPage,
+      currentPage * moviesPerPage
+    )
+    // Handle Page Change
+    const handlePageChange = (pageNumber) =>{
+      setCurrentPage(pageNumber);
+    }
+
+    // Fetch the api data
     useEffect(()=>{
       fetch("/api/topmovies")
       .then(
@@ -25,12 +37,27 @@ const TopMovies = () => {
     <div className='top-movies m-3 p-3'>
         <Container className='p-3'>
         <Row>
-          {movies.map((movie, index) => (
+          {currentMovies.map((movie, index) => (
             <Col key={index} xs={12} md={4} lg={4} className="mb-4">
               <MovieCard {...movie} />
             </Col>
           ))}
         </Row>
+        <Pagination className="justify-content-center">
+          <Pagination.First onClick={() => handlePageChange(1)} disabled={currentPage === 1} />
+          <Pagination.Prev onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} />
+          {[...Array(totalPages).keys()].map(pageNumber => (
+            <Pagination.Item
+              key={pageNumber + 1}
+              active={pageNumber + 1 === currentPage}
+              onClick={() => handlePageChange(pageNumber + 1)}
+            >
+              {pageNumber + 1}
+            </Pagination.Item>
+          ))}
+          <Pagination.Next onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} />
+          <Pagination.Last onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages} />
+        </Pagination>
       </Container>
     </div>
   )
