4728db9d9b3e8694606cb8231eacfb06eaaa6696
samueldob <frankdobisamuel@gmail.com>
2024-12-13 13:28:27 +0100

    switched the api call to the front-end for genre lists

--- client/src/components/Genres.js
+++ client/src/components/Genres.js
@@ -8,16 +8,18 @@ const Genres = () => {
     const [movies, setMovies] = useState([]);
 
     useEffect(()=>{
-        
-        axios.get(`/api/genres/${genreId}`)
-        .then(
-            response => response.json()
-        )
-        .then(
-            data =>{
-                console.log(data.results)
-            }
-        )
+    
+            const fetchData = async () => {
+                try {
+                    const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=1ae5981ff7d4f8f7646cc506eebc1c91&with_genres=${genreId}`)  
+                   
+                    const genreList = response.data.results
+                    console.log(genreList)
+                } catch (error) {
+                    console.log('Error getting data', error)
+                }
+            } 
+            fetchData()
     },[genreId])
   return (
     <div className=''>
