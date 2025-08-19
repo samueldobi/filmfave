aed7db0501970ef02af8f8538c734f2fdde7fa6a
samueldob <frankdobisamuel@gmail.com>
2025-08-19 08:39:46 +0100

    test:  testing api key fixes

--- client/src/utilities/api.js
+++ client/src/utilities/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:5000/api';
 const api = axios.create({
- baseURL:  '/api' ,
+ baseURL:  BASE_URL ,
   headers: {
     "Content-Type": "application/json",
   },
--- server/api/popular.js
+++ server/api/popular.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 module.exports = async (req, res) =>{
     const apiKey = process.env.TMDB_API_KEY; 
+    if(!apiKey){
+        return res.status(500).json({success:false, message:"API key not found"});
+    }
     try{
         const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
             res.status(200).json({
